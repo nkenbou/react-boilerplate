9aeb113c9f0766472a574a83573805b1269c2bf8
nkenbou <nkenbou@gmail.com>
2019-10-23 09:55:26 +0900

    create minimum react app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const outputPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    reactApp: [path.join(__dirname, 'src', 'index.js')]
+  },
+  output: {
+    path: outputPath,
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules')],
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: outputPath,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      hash: true
+    })
+  ]
+};
